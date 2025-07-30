49bfa84ec4157d51d01861778c86fef7d3d1a030
Ha young, Kim <74491172+hayoung-jeremy@users.noreply.github.com>
2025-07-31 02:21:04 +0900

    chore: move XM3 transparent models into same folder

--- components/3d/assets/XM3/XM3_container.tsx
+++ components/3d/assets/XM3/XM3_container.tsx
@@ -1,12 +1,14 @@
 import { degToRad } from "three/src/math/MathUtils.js";
 
-import XM3_body_transparent from "./XM3_body_transparent";
-import XM3_bonnet_transparent from "./XM3_bonnet_transparent";
-import XM3_bumper_transparnet from "./XM3_bumper_transparent";
-import XM3_headlight_transparent from "./XM3_headlight_transparent";
-import XM3_taillamp_transparent from "./XM3_taillamp_transparent";
-import XM3_wheel_transparent from "./XM3_wheel_transparent";
 import useDisplay from "@/hooks/useDisplay";
+import {
+  XM3_body_transparent,
+  XM3_bonnet_transparent,
+  XM3_bumper_transparent,
+  XM3_headlight_transparent,
+  XM3_taillamp_transparent,
+  XM3_wheel_transparent,
+} from "./transparent";
 
 const XM3_container = () => {
   const { isDesktop } = useDisplay();
@@ -15,7 +17,7 @@ const XM3_container = () => {
     <group scale={isDesktop ? 0.0011 : 0.0007} position={[0, -1, 0]} rotation={[0, degToRad(10), 0]}>
       <XM3_body_transparent />
       <XM3_bonnet_transparent />
-      <XM3_bumper_transparnet />
+      <XM3_bumper_transparent />
       <XM3_headlight_transparent />
       <XM3_taillamp_transparent />
       <XM3_wheel_transparent />
---
+++ components/3d/assets/XM3/transparent/index.ts
@@ -0,0 +1,6 @@
+export { default as XM3_body_transparent } from "./XM3_body_transparent";
+export { default as XM3_bonnet_transparent } from "./XM3_bonnet_transparent";
+export { default as XM3_bumper_transparent } from "./XM3_bumper_transparent";
+export { default as XM3_headlight_transparent } from "./XM3_headlight_transparent";
+export { default as XM3_taillamp_transparent } from "./XM3_taillamp_transparent";
+export { default as XM3_wheel_transparent } from "./XM3_wheel_transparent";
