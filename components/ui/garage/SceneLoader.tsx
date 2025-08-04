d0c53a6c08a6d3f41257384e03f031efa892780e
Ha young, Kim <74491172+hayoung-jeremy@users.noreply.github.com>
2025-08-04 21:40:22 +0900

    chore: update scene loader's font weight

--- components/ui/garage/SceneLoader.tsx
+++ components/ui/garage/SceneLoader.tsx
@@ -4,7 +4,7 @@ const SceneLoader = () => {
   const { progress } = useProgress();
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black text-white/40 font-bold text-xl xl:text-4xl">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black text-white/40 font-medium text-xl xl:text-4xl">
       Loading... {Math.floor(progress >= 99 ? 99 : progress)}%
     </div>
   );
