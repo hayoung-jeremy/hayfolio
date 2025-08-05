38985a956dc9869b365d8062284588c2dcc85305
Ha young, Kim <74491172+hayoung-jeremy@users.noreply.github.com>
2025-08-05 13:00:57 +0900

    chore: remove unused files

--- components/3d/assets/background/index.ts
+++ components/3d/assets/background/index.ts
@@ -1,3 +1,2 @@
 export { default as Modern_garage_1k } from "./Modern_garage_1k";
 export { default as Modern_garage_4k } from "./Modern_garage_4k";
-export { default as Modern_garage_8k } from "./Modern_garage_8k";
